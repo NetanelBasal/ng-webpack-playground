4de4b07cfa20940203eb4296aef61484c165e716
Netanel Basal <netanel7799@gmail.com>
2019-09-07 21:55:59 +0300

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyDJkOis4paCvfMrL4cLgkQXfgveiGwFQKw',
-    projectId: 'akita-4bece',
-  }
+  production: false
 };
 
 /*
